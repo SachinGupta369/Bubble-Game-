33212e15ddc64e5f9307a905986a32346ce25620
Sachin Gupta <sachingupta0098@gmail.com>
2024-05-25 17:54:06 +0530

    show score

--- script.js
+++ script.js
@@ -1,57 +1,53 @@
-let bubbly = ""
-let secondss=60
-let scoreVal = 0
-let getNewHitRn = 0
+let bubbly = "";
+let secondss = 3;
+let scoreVal = 0;
+let getNewHitRn = 0;
 
 // Make bubbles functions
-function makebubbles(){
-
-    for(let i=0;i<96;i++){
-        let count = Math.floor(Math.random()*10);
-        bubbly +=`<div class="bubble">${count}</div>`;
-    }
-    
-    document.querySelector(".btm").innerHTML=bubbly;
+function makebubbles() {
+  for (let i = 0; i < 96; i++) {
+    let count = Math.floor(Math.random() * 10);
+    bubbly += `<div class="bubble">${count}</div>`;
+  }
+  document.querySelector(".btm").innerHTML = bubbly;
 }
 
-// Timer functions 
-function runTimer(){
-      let clearTimer= setInterval(function(){
-        if(secondss>0){
-            secondss--
-            document.querySelector("#timeVal").textContent = secondss;
-        }else{
-           // document.querySelector(".btm").innerHTML=`<h1> Game Over <h1/>`
-        }
-        },1000)  
+// Timer functions
+function runTimer() {
+  let clearTimer = setInterval(function () {
+    if (secondss > 0) {
+      secondss--;
+      document.querySelector("#timeVal").textContent = secondss;
+    } else {
+      document.querySelector(".btm").innerHTML =
+        `<h1> Game Over <h1/>` + `<h1> Score is ${scoreVal} <h1/>`;
+    }
+  }, 1000);
 }
 
 //Score functions
-function score(){
-    scoreVal += 10;
-    document.querySelector("#scoreHtml").textContent=scoreVal
+function score() {
+  scoreVal += 10;
+  document.querySelector("#scoreHtml").textContent = scoreVal;
 }
 
 //getNewHit functions
-function getNewHit(){
- getNewHitRn = Math.floor(Math.random()*10)
-    document.querySelector("#hitHtml").textContent= getNewHitRn
+function getNewHit() {
+  getNewHitRn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitHtml").textContent = getNewHitRn;
 }
 
 // Click Hits functions
-    document.querySelector(".btm").addEventListener("click",function(ccss){
-    let clickdata = Number(ccss.target.textContent)  
-    if(clickdata === getNewHitRn){
-        score()
-        makebubbles()
-        getNewHit()
-    }
- });
+document.querySelector(".btm").addEventListener("click", function (ccss) {
+  let clickdata = Number(ccss.target.textContent);
+  if (clickdata === getNewHitRn) {
+    score();
+    makebubbles();
+    getNewHit();
+  }
+});
 
-
-
-getNewHit()
+getNewHit();
 //score()
-runTimer()
-makebubbles()
-
+runTimer();
+makebubbles();
